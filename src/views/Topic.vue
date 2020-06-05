4a9b50e399f3a00d72bb6b7ea76adbd01971d5e1
qwxying <qwxying@gmail.com>
2020-06-05 22:16:04 +0800

    css媒体查询布局

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -2,7 +2,7 @@
   <div>
     <Loading v-if="this.loading"/>
     <Layout v-else>
-      <el-main>
+      <el-main class="main">
         <el-card class="topic">
           <div slot="header" class="topic_title">
             <div class="topic_title">{{topic.title}}</div>
@@ -15,9 +15,14 @@
               <li>• 来自{{topic | tabFormatter}}</li>
             </ul>
           </div>
-          <div v-html="topic.content" class="topic_content"></div>
+          <div class="markdown-body">
+            <div v-html="topic.content" class="topic_content"></div>
+          </div>
         </el-card>
       </el-main>
+      <el-aside width="25%">
+        <h1>666</h1>
+      </el-aside>
     </Layout>
   </div>
 </template>
@@ -25,6 +30,7 @@
 <script>
   import Loading from "../components/Loading"
   import Layout from "../components/Layout"
+  import "github-markdown-css"
 
   export default {
     name: "Topic",
@@ -42,6 +48,7 @@
               if (res.data.success) {
                 this.loading = false
                 this.topic = res.data.data
+                console.log(this.topic.content)
               }
             }
           )
@@ -58,5 +65,28 @@
 </script>
 
 <style>
-  @import url('../assets/markdown-github.css');
+  .el-aside {
+    border: 1px solid red;
+  }
+
+  .markdown-body {
+    box-sizing: border-box;
+    width: 100%;
+    margin: 0 auto;
+    padding: 45px;
+  }
+
+  @media (max-width: 767px) {
+    .main, .markdown-body {
+      padding: 0 !important;
+    }
+
+    .el-aside {
+      display: none;
+    }
+  }
+
+  .markdown-text > * {
+    text-align: left;
+  }
 </style>
