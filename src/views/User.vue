fe26e5929026054f2458704472894ac206ea4a4b
qwxying <qwxying@gmail.com>
2020-06-08 12:50:45 +0800

    用户界面完成 User.vue

--- src/views/User.vue
+++ src/views/User.vue
@@ -4,33 +4,45 @@
     <Loading v-if="loading"/>
     <Layout v-else>
       <el-main>
+        <!--        用户基本信息-->
         <el-card>
-          <img class="own_img" :src="user.avatar_url" alt="">
-          <span>{{user.loginname}}</span>
-          <span>{{user.score}} 积分</span>
-          <span>注册时间：{{user.create_at|formatDate}}</span>
+          <a slot="header" class="to_home" href="/">主页/</a>
+          <div class="user_info">
+            <img class="own_img" :src="user.avatar_url" alt="">
+            <span>{{user.loginname}}</span>
+          </div>
+          <p class="score">{{user.score}} 积分</p>
+          <p class="registration_time">注册时间：{{user.create_at|formatDate}}</p>
         </el-card>
+        <!--        用户近期文章-->
         <el-card>
-          <div class="recent_topic" v-for="(recent_topic,index) in user.recent_topics" :key="index">
-            <img :src="recent_topic.author.avatar_url" alt="">
-            <span>
+          <div slot="header">用户近期文章</div>
+          <ul>
+            <li v-for="(recent_topic,index) in user.recent_topics.slice(0,5)" :key="index">
+              <img :src="recent_topic.author.avatar_url" alt="">
+              <router-link :to="{path:`/topic/${recent_topic.id}`}">
                 {{recent_topic.title}}
-              </span>
-            <span>
-              {{recent_topic.last_reply_at}}
-            </span>
-          </div>
+              </router-link>
+              <span class="reply_time">{{recent_topic.last_reply_at|formatDate}}</span>
+            </li>
+          </ul>
         </el-card>
+        <!--        用户近期回复-->
         <el-card>
-          <div class="recent_reply" v-for="(recent_reply,index) in user.recent_replies" :key="index">
-            <img :src="recent_reply.author.avatar_url" alt="">
-            <span>
+          <div slot="header">用户回复的文章</div>
+          <ul>
+            <li class="reply" v-for="(recent_reply,index) in user.recent_replies.slice(0,5)" :key="index">
+              <router-link class="img" :to="{path:`/user/${recent_reply.author.loginname}`}">
+                <img :src="recent_reply.author.avatar_url" alt="">
+              </router-link>
+              <router-link :to="{path:`/topic/${recent_reply.id}`}">
                 {{recent_reply.title}}
-              </span>
-            <span>
-              {{recent_reply.last_reply_at}}
+              </router-link>
+              <span class="reply_time">
+              {{recent_reply.last_reply_at|formatDate}}
             </span>
-          </div>
+            </li>
+          </ul>
         </el-card>
       </el-main>
     </Layout>
@@ -67,6 +79,10 @@
           })
       }
     },
+    watch: {
+      $route: "getData"
+    }
+    ,
     beforeMount() {
       this.loading = true
       this.getData()
@@ -75,6 +91,89 @@
 </script>
 
 <style scoped>
+
+  .to_home {
+    color: #80bd01;
+  }
+
+  .reply_time {
+    font-size: 11px;
+    color: #777;
+    white-space: nowrap;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    border-bottom: 1px solid #f0f0f0;
+  }
+
+  .reply .img {
+    padding: 0;
+    margin: 0;
+    height: 30px;
+    width: 30px;
+  }
+
+  .el-card li > a {
+    max-width: 100%;
+    font-size: 16px;
+    line-height: 30px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-all;
+    white-space: nowrap;
+    color: #0088cc;
+    margin-right: auto;
+    padding-left: 12px;
+  }
+
+  .el-card li > a:hover {
+    color: #005580;
+    text-decoration: underline;
+  }
+
+
+  .el-card {
+    margin-bottom: 16px;;
+  }
+
+
+  .score {
+    font-size: 14px;
+    padding: 4px;
+  }
+
+  .registration_time {
+    font-size: 14px;
+    padding: 4px;
+    color: #ababab;
+  }
+
+  .user_info {
+    display: flex;
+    align-items: center;
+  }
+
+  .user_info > img {
+    height: 40px;
+    width: 40px;
+    border-radius: 3px;
+  }
+
+  .user_info > span {
+    color: #778087;
+    margin-left: 16px;
+  }
+
+  img {
+    height: 30px;
+    width: 30px;
+    border-radius: 3px;
+  }
+
+
   .el-main {
     max-width: 70%;
     margin: 0 auto;
@@ -87,24 +186,8 @@
     }
   }
 
-  .el-card {
-    margin-bottom: 16px;
-  }
-
-  .el-card * {
-    display: flex;
-    flex-direction: column;
+  .el-main * {
     text-align: start;
   }
 
-  div img {
-    display: block;
-    width: 30px;
-    height: 30px;
-  }
-
-  .own_img {
-    width: 40px;
-    height: 40px;
-  }
 </style>
